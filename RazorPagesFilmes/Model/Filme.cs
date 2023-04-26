713ff773661b5d3cbc857c1cfbf085ba0dfdeaf7
Maikon William <maikonwilliam08@gmail.com>
2023-04-26 17:59:22 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesFilmes/Model/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesFilmes.Model
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+        //Se não quiser iniciar nula a propriedade podemos inserir o valor vazio nela com string.Empty
+        public string Title { get; set; } = string.Empty;
+        //utilização do DataAnnotations para especificar o parâmetro que deseja informar no DateTime        
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; } = string.Empty;
+        public double Price { get; set; }
+
+
+    }
+}
